3fbc8107b6cee0b0ed183585780e378e41a2fd92
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-25 14:06:47 +0100

    GET / registered

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("public"));
+//app.use(express.static("public"));
 require("./routing/index")(app);
 
 app.listen(PORT, () => {
--- routing/index.js
+++ routing/index.js
@@ -1,3 +1,7 @@
+const renderMW = require("../middleware/renderMW");
+
 module.exports = (app) => {
   const objRepo = {};
+
+  app.get("/", renderMW(objRepo, "index"));
 };
