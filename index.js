f822f863245967b2825d65a3460cbeec5ba53ec6
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-25 13:41:36 +0100

    Routes imported into index

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.static("public"));
+require("./routing/index")(app);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
