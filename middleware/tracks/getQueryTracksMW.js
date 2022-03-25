5a8e828b74c9b93d2840dea1f9acf13b91850ca6
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-25 17:56:13 +0100

    Mock data added to getQueryTracksMW

--- middleware/tracks/getQueryTracksMW.js
+++ middleware/tracks/getQueryTracksMW.js
@@ -5,6 +5,21 @@
 
 module.exports = (objRepo) => {
   return (req, res, next) => {
+    res.locals.tracks = [
+      {
+        _id: 1,
+        title: "Rise",
+        artist: "Skillet",
+        description: "One of the greatest songs on the album Rise",
+      },
+      {
+        _id: 2,
+        title: "Rise",
+        artist: "Skillet",
+        description: "One of the greatest songs on the album Rise",
+      },
+    ];
+
     return next();
   };
 };
