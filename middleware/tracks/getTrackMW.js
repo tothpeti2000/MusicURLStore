5b383a3ca18904b0bd1cb9b7f92e203ba5d547ab
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-25 17:14:00 +0100

    getTrackMW comment added

--- middleware/tracks/getTrackMW.js
+++ middleware/tracks/getTrackMW.js
@@ -1,3 +1,8 @@
+/**
+ * Get the track with the given ID from the DB
+ * Save the result to res.locals.track
+ */
+
 module.exports = (objRepo) => {
   return (req, res, next) => {
     return next();
