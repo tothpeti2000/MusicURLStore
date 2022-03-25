1b4baec29d713778c9d44957aefe0931dbe39a97
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-25 20:33:07 +0100

    Playlist delete initial implementation added

--- middleware/playlists/deletePlaylistMW.js
+++ middleware/playlists/deletePlaylistMW.js
@@ -6,6 +6,13 @@
 
 module.exports = (objRepo) => {
   return (req, res, next) => {
+    // Delete the playlist from objRepo
+    const idx = objRepo.playlists.find(
+      (p) => p._id === res.locals.playlist._id
+    );
+
+    objRepo.playlists = objRepo.playlists.splice(idx, 1);
+
     return res.redirect("/playlists");
   };
 };
