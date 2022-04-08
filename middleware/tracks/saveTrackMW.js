e2d20c576f953bd5e3069611a7c35d6934e09ea3
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-04-08 15:14:56 +0200

    TrackEditNewForm issues fixed

--- middleware/tracks/saveTrackMW.js
+++ middleware/tracks/saveTrackMW.js
@@ -7,8 +7,30 @@
 
 module.exports = (objRepo) => {
   return (req, res, next) => {
-    if (typeof req.body === "undefined") {
+    if (
+      typeof req.body.url === "undefined" ||
+      typeof req.body.title === "undefined" ||
+      typeof req.body.artist === "undefined" ||
+      typeof req.body.description === "undefined"
+    ) {
       return next();
+    }
+
+    if (typeof res.locals.track === "undefined") {
+      const track = {
+        _id: "ObjectID_T4",
+        url: req.body.url,
+        title: req.body.title,
+        artist: req.body.artist,
+        description: req.body.description,
+      };
+
+      objRepo.tracks.push(track);
+    } else {
+      res.locals.track.url = req.body.url;
+      res.locals.track.title = req.body.title;
+      res.locals.track.artist = req.body.artist;
+      res.locals.track.description = req.body.description;
     }
 
     return res.redirect("/tracks");
