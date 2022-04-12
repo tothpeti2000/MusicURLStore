fd7558aa3fc9b215572431b2c91e3a53341b4ec6
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-04-12 23:11:35 +0200

    Track initial schema added

---
+++ models/track.js
@@ -0,0 +1,9 @@
+const { Schema } = require("mongoose");
+const db = require("./db/db");
+
+const Track = db.Model("Track", {
+  title: String,
+  url: String,
+  artist: String,
+  description: String,
+});
