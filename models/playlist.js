b1bcf67f22d9074a250f62d3d0e29c941af08298
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-04-25 16:30:15 +0200

    Playlist schema modified

--- models/playlist.js
+++ models/playlist.js
@@ -3,7 +3,10 @@ const db = require("../db/db");
 
 const Playlist = db.model("Playlist", {
   name: String,
-  img: String,
+  img: {
+    data: Buffer,
+    contentType: String,
+  },
   _tracks: [{ type: Schema.Types.ObjectId, ref: "Track" }],
 });
 
