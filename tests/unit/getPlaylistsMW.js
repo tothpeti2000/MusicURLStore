6a94a48b5d753798cd24c5188b2a6bc07f4564c4
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-05-14 19:06:43 +0200

    Unit tests added for getPlaylistsMW

---
+++ tests/unit/getPlaylistsMW.js
@@ -0,0 +1,49 @@
+const expect = require("chai").expect;
+var getPlaylistsMW = require("../../middleware/playlists/getPlaylistsMW");
+
+describe("getPlaylistsMW", () => {
+  it("should return all playlists", async () => {
+    const req = {};
+    const res = {
+      locals: {},
+    };
+
+    const next = (err) => {
+      expect(res.locals.playlists).to.eql(["Playlist1", "Playlist2"]);
+      expect(err).to.eql(undefined);
+    };
+
+    const objRepo = {
+      playlistModel: {
+        find: (filter) => {
+          return ["Playlist1", "Playlist2"];
+        },
+      },
+    };
+
+    await getPlaylistsMW(objRepo)(req, res, next);
+  });
+
+  it("should return error when a DB error occurs", async () => {
+    const req = {};
+    const res = {};
+
+    const next = (err) => {
+      expect(err.message).to.eql(
+        "An error occurred while trying to process your request"
+      );
+    };
+
+    const objRepo = {
+      playlistModel: {
+        find: (filter) => {
+          throw new Error(
+            "An error occurred while trying to process your request"
+          );
+        },
+      },
+    };
+
+    await getPlaylistsMW(objRepo)(req, res, next);
+  });
+});
